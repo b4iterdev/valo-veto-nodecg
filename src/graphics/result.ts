707c30bdb57056f7d464bac355d18adcb400ed61
b4iterdev <thaimeo200389@gmail.com>
2025-02-12 22:49:27 +0700

    add topinfo in result

--- src/graphics/result.ts
+++ src/graphics/result.ts
@@ -1,5 +1,22 @@
+import { update } from "lodash";
+
 const data = nodecg.Replicant('result');
 
 data.on('change', (newVal:any) => {
     console.log(newVal);
+    update();
 });
+
+function update() {
+    const result = data.value;
+    let leftLogo = document.getElementById('team-logo-left') as HTMLImageElement;
+    let rightLogo = document.getElementById('team-logo-right') as HTMLImageElement;
+    let leftName = document.getElementById('team-name-left') as HTMLDivElement;
+    let rightName = document.getElementById('team-name-right') as HTMLDivElement;
+    let bestOf = document.getElementById('bestOf') as HTMLDivElement;
+    leftLogo.src = result.data.leftLogo !== "" ? result.data.leftLogo : undefined;
+    rightLogo.src = result.data.rightLogo !== "" ? result.data.rightLogo : undefined;
+    leftName.innerHTML = result.data.leftTeam;
+    rightName.innerHTML = result.data.rightTeam;
+    bestOf.innerHTML = `Best of ${result.data.bestOf}`;
+}
